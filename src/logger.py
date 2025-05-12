3217d6c7252dc2eed65bf76b6a16b3fd92510edb
Hridoy Khan <rkhridoyinfo@gmail.com>
2025-05-12 19:18:56 +0600

    exception customization file is ready to use

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import os, logging
+from datetime import datetime
+
+
+FILE_DIR = 'logs'
+FILE_DIR = os.path.join(os.getcwd(),FILE_DIR)
+
+os.makedirs(FILE_DIR,exist_ok=True)
+
+CURRENT_TIME = datetime.now().strftime("%Y-%m-%d-%H:%M")
+FILE_NAME = f"log_{CURRENT_TIME}.log"
+
+LOG_FILE = os.path.join(FILE_DIR,FILE_NAME)
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    filemode='w',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
